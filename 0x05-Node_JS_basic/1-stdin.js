1e769f99cdc6add21ffd50c67d653dd619b2593b
Mochatr <mohamedchatr8@gmail.com>
2024-06-18 21:17:12 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+// This program will be executed through the command line
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+	const name = data.toString().trim();
+	console.log(`Your name is: ${name}`);
+	console.log('This important software is now closing');
+	process.exit();
+});
