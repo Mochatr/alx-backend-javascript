8a16b38d25dabd21c0ebc30f76dba4858617a426
Mochatr <mohamedchatr8@gmail.com>
2024-06-18 21:27:01 +0000

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 // This program will be executed through the command line
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
 	const name = data.toString().trim();
-	console.log(`Your name is: ${name}`);
-	console.log('This important software is now closing');
+	process.stdout.write(`Your name is: ${name}\n`);
+	process.stdout.write('This important software is now closing\n');
 	process.exit();
 });
