08dec66c7e314292c6505513e333e3a855c3934c
Mochatr <mohamedchatr8@gmail.com>
2024-06-21 00:20:03 +0000

    update

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,6 +1,6 @@
 const Utils = require("./Utils");
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalPayment = Utils.calculateNumner('SUM', totalAmount, totalShipping);
+  const totalPayment = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalPayment}`);
 };
 
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,7 +3,7 @@ const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 const sendPaymentRequestToApi = require('./4-payment');
-const Utils = require("./utils");
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function () {
   it('should usage of the Utils', function ()  {
