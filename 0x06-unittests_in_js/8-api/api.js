6903098acc79e223e4561f554e738002a26d892c
Mochatr <mohamedchatr8@gmail.com>
2024-06-21 14:23:33 +0000

    update

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -7,8 +7,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`API available on localhost port ${port}`)
 });
 
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,29 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
+let server;
+
+before((done) => {
+    server = require('./api');
+    server.on('listening', () => done());
+});
+
+after((done) => {
+    server.close(done);
+});
+
 
 describe('index page', () => {
-    before((done => {
-        app.listen(7865, () => {
-	    console.log('Test server runnningon port 7865');
-	    done();
+    it('should return status code 200', (done) => {
+        request.get('http://localhost:7865', (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
 	});
     });
 
-    it('should return status code 200', (done) => {
-        request.get('http://localhost:7865', (error, response, body) => {
-	    expect(response.statusCode).to.equal(200);
-	    done();
-        });
-    });
-
     it('should return the correct message', (done) => {
-	request.get('http://localhost:7865', (error, response, body) => {
-            expecte(body).to.equal('Welcome to the payment system');
-            done();
+        request.get('http://localhost:7865', (error, response, body) => {
+	    expect(body).to.equal('Welcome to the payment system');
+	    done();
         });
     });
 });
