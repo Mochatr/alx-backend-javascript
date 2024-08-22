68dbb18c5a6de18f8505ae24517e4605dbc3d879
Mochatr <mohamedchatr8@gmail.com>
2024-08-22 03:40:23 +0100

    Updated test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,53 +1,35 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-const { it } = require('node:test');
 
 describe('calculateNumber', () => {
-    // Edge case 1: a is an integer, b is an integer.
-    it('should return 4', () => {
-        const result = calculateNumber(1, 3);
-        assert.strictEqual(result, 4);
+    it('should return 4 when inputs are (1, 3)', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
     });
 
-    // Edge case 2: a is an integer, b is a float.
-    it('should return 5', () => {
-        const result = calculateNumber(1, 3.7);
-        assert.strictEqual(result, 5);
+    it('should return 5 when inputs are (1, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
 
-    // Edge case 3: a is a float, b is a float.
-    it('should return 5', () => {
-        const result = calculateNumber(1.2, 3.7);
-        assert.strictEqual(result, 5);
+    it('should return 5 when inputs are (1.2, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
 
-    // Edge case 4: a is a float, b is a float.
-    it ('should return 6', () => {
-        const result = calculateNumber(1.5, 3.7);
-        assert.strictEqual(result, 6);
+    it('should return 6 when inputs are (1.5, 3.7)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
 
     // Additional edge cases
-    it ('should return 0', () => {
-        const result = calculateNumber(0.1, 0.2);
-        assert.strictEqual(result, 0);
-    });
-
-    // Handling negative numbers
-    it ('should return negative numbers correctly', () => {
-        const result = calculateNumber(-1.6, -2.6);
-        assert.strictEqual(result, -5);
+    it ('should return 0 when inputs are (0.1, 0.2)', () => {
+        assert.strictEqual(calculateNumber(0.1, 0.2), 0);
     });
 
     // Handling negative and positive numbers
-    it ('should return 3', () => {
-        const result = calculateNumber(-3.4, 5.6);
-        assert.strictEqual(result, 3);
+    it ('should handle negative numbers correctly', () => {
+        assert.strictEqual(calculateNumber(-1.4, -3.7), -5);
     });
 
     // Handling negative and positive numbers
-    it ('should return -2', () => {
-        const result = calculateNumber(1.5, -3.7);
-        assert.strictEqual(result, -2);
+    it ('should handle mixed positive and negative numbers', () => {
+        assert.strictEqual(calculateNumber(1.5, -3.7), -2);
     });
 });
